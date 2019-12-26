bb5a89bb48e685098e01b4fb1a870020010a6f0e
Tyler Oalman <tyleroalman@gmail.com>
2019-12-26 01:56:28 -0600

    Point page table entries to each other

--- boot.asm
+++ boot.asm
@@ -3,6 +3,30 @@ global start
 section .text
 bits 32
 start:
+    ; Point the first entry of the level 4 page table to the first entry in the
+    ; p3 table
+    mov eax, p3_table
+    or eax, 0b11
+    mov dword [p4_table + 0], eax
+
+    ; Point the first entry of the level 3 page table to the first entry in the
+    ; p2 table
+    mov eax,p2_table
+    or eax, 0b11
+    mov dword [p3_table + 0], eax
+
+    ; point each page table level two entry to a page
+    mov ecx, 0          ; counter variable
+.map_p2_table:
+    mov eax, 0x200000   ; 2MiB
+    mul ecx
+    or eax, 0b10000011
+    mov [p2_table + ecx * 8], eax
+
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2_table
+
     mov word [0xb8000], 0x0248 ; H
     mov word [0xb8002], 0x0265 ; e
     mov word [0xb8004], 0x026c ; l
@@ -18,12 +42,12 @@ start:
     mov word [0xb8018], 0x0221 ; !
     hlt
 
-section .bss
+section .bss ; bss = 'block started by symbol'
 
-align 4096
+align 4096 ; aligned to 4096 byte chunks
 
 p4_table:
-resb 4096
+    resb 4096 ; reserves bytes
 p3_table:
     resb 4096
 p2_table:
