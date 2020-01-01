ebe83ccc7dbb0b06201140f627c0ddbf8081cdff
Tyler Oalman <tyleroalman@gmail.com>
2020-01-01 16:55:04 -0600

    Jump into long mode

--- boot.asm
+++ boot.asm
@@ -52,19 +52,15 @@ start:
     ; load global descriptor table
     lgdt [gdt64.pointer]
 
-    mov word [0xb8000], 0x0248 ; H
-    mov word [0xb8002], 0x0265 ; e
-    mov word [0xb8004], 0x026c ; l
-    mov word [0xb8006], 0x026c ; l
-    mov word [0xb8008], 0x026f ; o
-    mov word [0xb800a], 0x022c ; ,
-    mov word [0xb800c], 0x0220 ;
-    mov word [0xb800e], 0x0277 ; w
-    mov word [0xb8010], 0x026f ; o
-    mov word [0xb8012], 0x0272 ; r
-    mov word [0xb8014], 0x026c ; l
-    mov word [0xb8016], 0x0264 ; d
-    mov word [0xb8018], 0x0221 ; !
+    ; update selectors
+    mov ax, gdt64.data  ; ax is a sixteen-bit register
+    mov ss, ax          ; ss is the stack segment register
+    mov ds, ax          ; ds is the data segment register
+    mov es, ax          ; es is an extra segment register
+
+    ; jump to long mode
+    jmp gdt64.code:long_mode_start
+
     hlt
 
 section .bss ; bss = 'block started by symbol'
@@ -78,11 +74,20 @@ p2_table:
 
 section .rodata
 gdt64:
-    dq 0
+    dq 0    ; zero entry
 .code: equ $ - gdt64
     dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
 .data: equ $ - gdt64
     dq (1<<44) | (1<<47) | (1<<41)  ; data segment
 .pointer:
-    dq .pointer - gdt64 - 1
+    dw .pointer - gdt64 - 1
     dq gdt64
+
+section .text
+bits 64
+long_mode_start:
+
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+
+    hlt
