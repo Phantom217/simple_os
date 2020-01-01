e4ca661cff902c3ef2e168b23e339438992e78d2
Tyler Oalman <tyleroalman@gmail.com>
2020-01-01 14:27:23 -0600

    Enable paging

--- boot.asm
+++ boot.asm
@@ -27,6 +27,27 @@ start:
     cmp ecx, 512
     jne .map_p2_table
 
+    ; move page table address to cr3
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit
+    mov ecx, 0xC0000080
+    rdmsr       ; read 'model specific register'
+    or eax, 1 << 8
+    wrmsr       ; write 'model specific register'
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
+
     mov word [0xb8000], 0x0248 ; H
     mov word [0xb8002], 0x0265 ; e
     mov word [0xb8004], 0x026c ; l
