1dcc7e6091405234073aa024bdc789fc56f1f5fa
Tyler Oalman <tyleroalman@gmail.com>
2020-01-03 00:47:56 -0600

    Make text "OKAY" be displayed through Rust code

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,17 @@
 extern fn eh_personality() {}
 
 #[lang = "panic_impl"]
-#[no_mangle]
 pub extern fn rust_begin_panic(_msg: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern fn kmain() -> ! {
+    unsafe {
+        let vga = 0xb8000 as *mut u64;
+
+        *vga = 0x2f592f412f4b2f4f;
+    };
+
     loop {}
 }
